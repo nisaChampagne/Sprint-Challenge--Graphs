fd2f052a8621caff8ad232f3a18c1a691b32199e
Nisa Champagne <nchampag23@gmail.com>
2020-02-21 11:35:43 -0500

    transfering solution to sprint repo

--- adv.py
+++ adv.py
@@ -1,9 +1,43 @@
 from room import Room
 from player import Player
 from world import World
+from util import Graph, Stack, Queue
+
+
 
 import random
 from ast import literal_eval
+
+PURPLE = "\033[95m"
+CYAN ="\033[96m"
+DARKCYAN = "\033[36m"
+BLUE = "\033[94m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+RED = "\033[91m"
+BOLD = "\033[1m"
+UNDERLINE = "\033[4m"
+END = "\033[0m"
+LIGHTRED     = "\033[91m"
+LIGHTGREEN   = "\033[92m"
+LIGHTYELLOW  = "\033[93m"
+LIGHTBLUE    = "\033[94m"
+LIGHTMAGENTA = "\033[95m"
+LIGHTCYAN    = "\033[96m"
+BACKGROUNDRED          = "\033[41m"
+BACKGROUNDGREEN        = "\033[42m"
+BACKGROUNDYELLOW       = "\033[43m"
+BACKGROUNDBLUE         = "\033[44m"
+BACKGROUNDMAGENTA      = "\033[45m"
+BACKGROUNDCYAN         = "\033[46m"
+BACKGROUNDLIGHTGRAY    = "\033[47m"
+BACKGROUNDDARKGRAY     = "\033[100m"
+BACKGROUNDLIGHTRED     = "\033[101m"
+BACKGROUNDLIGHTGREEN   = "\033[102m"
+BACKGROUNDLIGHTYELLOW  = "\033[103m"
+BACKGROUNDLIGHTBLUE    = "\033[104m"
+BACKGROUNDLIGHTMAGENTA = "\033[105m"
+BACKGROUNDLIGHTCYAN    = "\033[106"
 
 # Load world
 world = World()
@@ -51,12 +85,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
---
+++ util.py
@@ -0,0 +1,54 @@
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, v_id):
+        self.vertices[v_id] = {}
+
+    def add_edge(self, v1, v2, direction):
+
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1][direction] = v2
+        else:
+            raise KeyError("One or both vertex not in graoh")
+
+    def get_neighbors(self, v_id):
+
+        if v_id in self.vertices:
+            return self.vertices[v_id]
+        else:
+            raise KeyError("Vertex not valid")
+
+
+class Queue:
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val):
+        self.stack.append(val)
+
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.stack)
